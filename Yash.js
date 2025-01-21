e557bf4f4b37f2aeb2a12891aa13667b1f1a6813
aktti <dwivedisanidhya55@gmail.com>
2025-01-21 11:39:04 +0530

    hello

--- Yash.js
+++ Yash.js
@@ -1,6 +1,20 @@
-const a = 58;
-const b = (a)=>{
-    return a;
-}
+// const a = 58;
+// const b = (a)=>{
+//     return a;
+// }
 
-console.log(b(a));
+// console.log(b(a));
+
+// let a = 40;
+// let b = 30;
+
+// console.log(`before swap a= ${a}`);
+// console.log(`before swap a= ${b}`);
+
+// // a would be swapped to b and b would be swapped to a
+// [b, a] = [a, b];
+
+// console.log(`after swap a= ${a}`);
+// console.log(`after swap a= ${b}`);
+
+
